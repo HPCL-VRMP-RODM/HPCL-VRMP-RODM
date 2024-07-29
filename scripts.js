b10c58cbed7acf4bd2cc22a732f55a14387fe58d
ManikantaVoonna <manikanta031@gmail.com>
2024-07-30 00:21:42 +0530

    added look and feel

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function turnOn() {
+    console.log('hello');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://script.google.com/macros/s/AKfycbx1ZiwqqLW6afLmRnGfHQqhgYPr3R2Yh063EIO3AtmFOrLL6Vx9Ok5Jc9yChtuxrV3u/exec?cmd=write&&cell=A1&&value=1', true);
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            console.log('Response:', xhr.responseText);
+        } else {
+            console.error('Request failed with status:', xhr.status);
+        }
+    };
+    xhr.send();
+}
+function turnOff() {
+    console.log('hello');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://script.google.com/macros/s/AKfycbx1ZiwqqLW6afLmRnGfHQqhgYPr3R2Yh063EIO3AtmFOrLL6Vx9Ok5Jc9yChtuxrV3u/exec?cmd=write&&cell=A1&&value=0', true);
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            console.log('Response:', xhr.responseText);
+        } else {
+            console.error('Request failed with status:', xhr.status);
+        }
+    };
+    xhr.send();
+}
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const yearSpan = document.getElementById('year');
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
